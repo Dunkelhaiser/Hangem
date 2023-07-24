d0e64bde143fe2922de12e7ee89b86b2a11a07f6
KaeserOfHonour <kaeser418@gmail.com>
2023-07-24 23:58:07 +0300

    Updated wrapper styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Hangman from "./features/Hangman";
 
 function App() {
     return (
-        <main className="dark:bg-dark-700 grid min-h-screen place-items-center gap-6 bg-stone-100 p-4">
+        <main className="dark:bg-dark-700  flex  min-h-screen flex-col items-center justify-center gap-6 bg-stone-100 p-4">
             <Hangman />
         </main>
     );
