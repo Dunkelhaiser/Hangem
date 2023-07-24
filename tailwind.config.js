93c21e295be57518d82448110c5d609d2d693d7e
KaeserOfHonour <kaeser418@gmail.com>
2023-07-24 19:35:50 +0300

    Initialized project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {
-            colors: {
-                dark: {
-                    300: "#333842",
-                    400: "#292d36",
-                    500: "#272b33",
-                    600: "#22252d",
-                    700: "#1a1c22",
-                    800: "#1c1f25",
-                },
-                red: {
-                    400: "#eb6666",
-                },
-            },
-            dropShadow: {
-                "3xl": ["0 25px 25px rgb(0 0 0 / 0.05)"],
-            },
-        },
-    },
     plugins: [],
     darkMode: "class",
 };
