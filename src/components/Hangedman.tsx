dde94f1640096fc69cc5fc146e2bc14628a1ed0a
KaeserOfHonour <kaeser418@gmail.com>
2023-07-25 13:02:57 +0300

    Changed hangman size

--- src/components/Hangedman.tsx
+++ src/components/Hangedman.tsx
@@ -5,20 +5,20 @@ interface Props {
 const Hangedman = ({ guesses }: Props) => {
     const stand = (
         <>
-            <div className="absolute right-0 top-0 h-12 w-2 rounded-md bg-zinc-700" />
-            <div className="ml-[124px] h-2 w-52 rounded-t-md bg-zinc-700" />
-            <div className="ml-[124px] h-96 w-2 bg-zinc-700" />
-            <div className="h-2 w-64 rounded-md bg-zinc-700" />
+            <div className="absolute right-0 top-0 h-10 w-2 rounded-md bg-zinc-700" />
+            <div className="ml-[87px] h-2 w-44 rounded-t-md bg-zinc-700" />
+            <div className="ml-[87px] h-80 w-2 bg-zinc-700" />
+            <div className="h-2 w-44 rounded-md bg-zinc-700" />
         </>
     );
-    const head = <div className="absolute right-[-24px] top-12 h-14 w-14 rounded-full border-8 border-zinc-700" />;
-    const body = <div className="absolute right-0 top-24 h-[105px] w-2 bg-zinc-700" />;
+    const head = <div className="absolute right-[-24px] top-10 h-14 w-14 rounded-full border-8 border-zinc-700" />;
+    const body = <div className="absolute right-0 top-[5.5rem] h-24 w-2 bg-zinc-700" />;
     const rightHand = (
-        <div className="absolute right-[-64px] top-32 h-2 w-16 origin-bottom-left -rotate-[30deg] rounded-r-md bg-zinc-700" />
+        <div className="absolute right-[-56px] top-32 h-2 w-14 origin-bottom-left -rotate-[30deg] rounded-r-md bg-zinc-700" />
     );
-    const leftHand = <div className="absolute right-[8px] top-32 h-2 w-16 origin-bottom-right rotate-[30deg] rounded-l-md bg-zinc-700" />;
-    const rightLeg = <div className="absolute right-[-73px] top-48 h-2 w-20 origin-bottom-left rotate-[60deg] rounded-r-md bg-zinc-700" />;
-    const leftLeg = <div className="absolute right-[1px] top-48 h-2 w-20 origin-bottom-right -rotate-[60deg] rounded-l-md bg-zinc-700" />;
+    const leftHand = <div className="absolute right-[8px] top-32 h-2 w-14 origin-bottom-right rotate-[30deg] rounded-l-md bg-zinc-700" />;
+    const rightLeg = <div className="absolute right-[-57px] top-44 h-2 w-16 origin-bottom-left rotate-[60deg] rounded-r-md bg-zinc-700" />;
+    const leftLeg = <div className="absolute right-[1px] top-44 h-2 w-16 origin-bottom-right -rotate-[60deg] rounded-l-md bg-zinc-700" />;
     return (
         <div className="relative">
             {stand}
